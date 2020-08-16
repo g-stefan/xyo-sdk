1221cd912c4318534fed13bd06f8c148ee5354cd
Grigore Stefan <g_stefan@yahoo.com>
2020-08-16 14:48:40 +0300

    update

--- sdk.setup.ubuntu.sh
+++ sdk.setup.ubuntu.sh
@@ -8,4 +8,12 @@ sudo apt-get -y install libbz2-dev
 sudo apt-get -y install libpng-dev
 sudo apt-get -y install libxml2-dev
 sudo apt-get -y install libxslt1-dev
-sudo apt-get -y install libssl1.0-dev
+
+RELEASE=`cat /etc/lsb-release | grep DISTRIB_RELEASE| cut -d "=" -f 2`
+
+if [ "$RELEASE" = "18.04" ]; then
+	sudo apt-get -y install libssl1.0-dev
+fi
+if [ "$RELEASE" = "20.04" ]; then
+	sudo apt-get -y install libssl-dev
+fi
