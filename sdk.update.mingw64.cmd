e8fab496bbfae5e6fb69cfd6f21f77c44d255dc2
Grigore Stefan <g_stefan@yahoo.com>
2020-08-17 00:40:35 +0300

    update

---
+++ sdk.update.mingw64.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+set CMD_MINGW64=C:\msys64\mingw64.exe
+
+%CMD_MINGW64% "./sdk.update.ubuntu.sh"
---
+++ sdk.update.wsl.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+set CMD_BASH=C:\Windows\System32\bash.exe
+
+%CMD_BASH% -c "./sdk.update.ubuntu.sh"
