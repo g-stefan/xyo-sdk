9998f8a15cee183263198d2c488abfc162d053ce
Grigore Stefan <g_stefan@yahoo.com>
2022-12-30 12:43:54 +0200

    Update

--- fabricare/sdk-release-remove-all.js
+++ fabricare/sdk-release-remove-all.js
@@ -3,20 +3,6 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
-// --- windows
-
-global.projectList = JSON.decode(Shell.fileGetContents("fabricare/source/windows.json"));
-
-forEachProject(function(project) {
-	runInPath("../" + project, function() {
-		Shell.removeDirRecursively("release");
-	});
-});
-
-// --- linux
-
-global.projectList = JSON.decode(Shell.fileGetContents("fabricare/source/linux.json"));
-
 forEachProject(function(project) {
 	runInPath("../" + project, function() {
 		Console.writeLn("- " + project);
