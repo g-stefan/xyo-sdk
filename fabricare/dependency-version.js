7e4eb546d741fcf32ed4be82cb17513199608394
Grigore Stefan <g_stefan@yahoo.com>
2024-01-02 22:55:07 +0200

    Update

--- fabricare/dependency-version.js
+++ fabricare/dependency-version.js
@@ -1,6 +1,6 @@
 // Created by Grigore Stefan <g_stefan@yahoo.com>
 // Public domain (Unlicense) <http://unlicense.org>
-// SPDX-FileCopyrightText: 2022-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-FileCopyrightText: 2022-2024 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
 Fabricare.include("library");
@@ -167,14 +167,6 @@ if (Application.hasFlag("commit")) {
 					};
 				};
 			};
-			runInPath("../" + project, function() {
-				if (Shell.system("fabricare make")) {
-					throw ("make");
-				};
-				if (Shell.system("fabricare install")) {
-					throw ("install");
-				};
-			});
 		};
 	});
 };
